1a49f1cf9a802d9c31113b4bfbed385d42535d2d
BMazarro <beachu_84@hotmail.com>
2014-10-24 20:12:49 +0200

    Videoclub Hibernate

---
+++ src/proyecto/Proyecto.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto;
+
+/**
+ *
+ * @author BEA
+ */
+public class Proyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Principal p= new Principal();
+        p.setVisible(true);
+    }
+}
